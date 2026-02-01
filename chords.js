36800e206feff5daa0e3acd42f901caa0f644c53
prem4sign <prem4sign@gmail.com>
2026-02-01 13:28:02 +0530

    Add files via upload

---
+++ chords.js
@@ -0,0 +1,30 @@
+// chords.js
+// Edit this file to add/remove chords
+
+const CHORDS = [
+  "F#m",
+  "Bb",
+  "Em",
+  "Db",
+  "Am",
+  "E",
+  "Cm",
+  "A",
+  "Gm",
+  "B",
+  "Dm",
+  "Ab",
+  "C",
+  "F",
+  "Bm",
+  "Eb",
+  "G",
+  "Fm",
+  "D",
+  "C#m",
+  "Bbm",
+  "F#",
+  "G#m",
+  "Em"
+];
+
